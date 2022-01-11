4ff9aa964f74d6cd19c2f3ffaa913b5d09bf6b90
Mansoor Akhter <79827179+MansoorAkhter@users.noreply.github.com>
2022-01-11 13:53:38 +0500

    Initialize project using Create React App

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./todoApp.css";
+
+const TodoApp = () => {
+
+  const [checkBoxx, setCheckBoxx] = useState()
+
+  const checkBox = () => {
+    console.log("adad");
+    setCheckBoxx(!checkBoxx);
+  };
+
+  return (
+    <div className="container">
+    
+
+
+      <h1>TODO APP</h1>
+      <div class="inputWrpr">
+        <input type="text" class="data" />
+
+        <button className="addBtn">Add</button>
+      </div>
+
+      <h3>TODAY'S TASKS</h3>
+
+      <ul>
+        <li>
+          <button className="checkbox" onClick={checkBox}>
+            {checkBoxx ? (
+              <i class="fas fa-check-circle"></i>
+            ) : (
+              <i class="far fa-circle"></i>
+            )}
+          </button>
+          <p>Meeting</p>
+          <div>
+            <i class="fas fa-edit"></i>
+            <i class="far fa-trash-alt"></i>
+          </div>
+        </li>
+      </ul>
+
+      <button className="clear">Clear</button>
+    </div>
+  );
+};
+
+export default TodoApp;
